a2928eb26a5cf87aa868b14e9d83171368ee9742
Renata Diaz <diaz.renatam@gmail.com>
2019-09-19 13:54:50 -0400

    try 10k and 40

--- analysis/pipeline.R
+++ analysis/pipeline.R
@@ -6,11 +6,11 @@ library(scadsplants)
 #expose_imports(scads)
 #expose_imports(scadsplants)
 
-years <-c(1983, 1985, 1986, 1988, 1991, 1993, 1994, 1995, 1996, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009)
+years <-c(1986, 1994, 1996, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2006, 2007, 2009)
 
 max_s <-40
 
-max_n <- 30000
+max_n <- 10000
 
 # get data
 datasets_plan <- drake_plan(
