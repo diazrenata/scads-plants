72b1ac902b54c2343f74b79119eed77ed2315718
Renata Diaz <diaz.renatam@gmail.com>
2019-10-08 13:09:24 -0400

    di functions, di report

--- R/skew_functions.r
+++ R/skew_functions.r
@@ -19,22 +19,75 @@ add_skew <- function(fs_samples_df) {
 
 #' Skewness
 #'
-#' @param fs_samples_df df of all samples, with columns for sim, year, season, treatment, source
+#' @param fs_samples_df df of all samples, with column for abundance
 #'
 #' @return summary of skewness per sample
 #' @export
 #'
 #' @importFrom e1071 skewness
-#' @importFrom dplyr group_by summarize ungroup
+#' @importFrom dplyr group_by_at summarize ungroup
 #' @importFrom vegan diversity
 add_dis <- function(fs_samples_df) {
 
+  groupvars <- colnames(fs_samples_df)[ which(colnames(fs_samples_df) != "abund")]
+
   sim_dis <- fs_samples_df %>%
-    dplyr::group_by(sim, year, season, treatment, source) %>%
+    dplyr::group_by_at(.vars = groupvars) %>%
     dplyr::summarize(skew = e1071::skewness(abund),
                      shannon = vegan::diversity(abund, index = "shannon", base = exp(1)),
                      simpson = vegan::diversity(abund, index = "simpson")) %>%
     dplyr::ungroup()
 
   return(sim_dis)
+}
+
+
+
+#' Get percentile values
+#'
+#' @param a_vector Vector of values
+#'
+#' @return Vector of percentile values for all values in the vector
+#' @export
+#'
+get_percentiles <- function(a_vector) {
+
+  nvals <- length(a_vector)
+
+  percentile_vals <- vapply(as.matrix(a_vector), FUN = count_below, a_vector = a_vector, FUN.VALUE = 100)
+
+  percentile_vals <- 100 * (percentile_vals / nvals)
+
+  return(percentile_vals)
+}
+
+
+#' Count values below a value
+#'
+#' @param a_value Focal value
+#' @param a_vector Vector for comparison
+#'
+#' @return Number of values in vector below value
+#' @export
+#'
+count_below <- function(a_value, a_vector) {
+  return(sum(a_vector < a_value))
+}
+
+#' Get one percentile value
+#'
+#' @param a_value Focal value
+#' @param a_vector Comparison vector
+#'
+#' @return Percentile of focal value within comparison vector
+#' @export
+get_percentile <- function(a_value, a_vector) {
+
+  count_below <- sum(a_vector < a_value)
+
+  nvals <- length(a_vector)
+
+  percentile_val <- 100 * (count_below / nvals)
+
+  return(percentile_val)
 }
--- analysis/reports/dis.md
+++ analysis/reports/dis.md
@@ -29,7 +29,7 @@ Skewness is not entirely unique to an element of the FS, but you don't get re-us
 
     ## Warning: Removed 1 rows containing missing values (geom_point).
 
-![](skewness_files/figure-markdown_github/dist%20of%20skew-1.png)
+![](dis_files/figure-markdown_github/dist%20of%20skew-1.png)
 
 2009 is problematic because the communities are very small (S = 1 and 3, N = 2 and 5 respectively). Similarly, winter of 2000 had 12 individuals of 2 species. Notably, winter 1996 had 34 individuals of 3 species, and this appears to be enough to get some interesting variation going.
 
@@ -45,7 +45,7 @@ If this holds (and perhaps even if it doesn't) perhaps it's worth re-investigati
     ## Warning in max(data$density): no non-missing arguments to max; returning -
     ## Inf
 
-![](skewness_files/figure-markdown_github/shannon%20violins-1.png)
+![](dis_files/figure-markdown_github/shannon%20violins-1.png)
 
 #### Simpson violins
 
@@ -55,12 +55,12 @@ If this holds (and perhaps even if it doesn't) perhaps it's worth re-investigati
     ## Warning in max(data$density): no non-missing arguments to max; returning -
     ## Inf
 
-![](skewness_files/figure-markdown_github/simspon%20violins-1.png)
+![](dis_files/figure-markdown_github/simspon%20violins-1.png)
 
-### Frequency of skewness percentiles
+### Frequency of diversity index percentiles
 
     ## Warning: Removed 2 rows containing non-finite values (stat_ydensity).
 
     ## Warning: Removed 2 rows containing missing values (geom_point).
 
-![](skewness_files/figure-markdown_github/skewness%20percentile%20hist-1.png)
+![](dis_files/figure-markdown_github/skewness%20percentile%20hist-1.png)
